2394c6a947cf71239d9bc32cc85eb2c4c658e619
bona-kim <bona.kim@outlook.be>
2019-08-25 19:43:07 +0200

    finish 5.9

--- js-complete-course/05-arrays/09-rand-array-stats/script.js
+++ js-complete-course/05-arrays/09-rand-array-stats/script.js
@@ -11,6 +11,33 @@
 
 (function() {
 
-    // your code here
+    // When you click the button,
+    document.getElementById("run").addEventListener("click", function() {
+
+        let numbers = [];
+        // display the ten elements of an array of 10 numbers (between 1 and 100)
+        for(let i = 1; i < 11; i++){        // Do this 10 times
+            //console.log(i);
+            let randomnr = Math.floor((Math.random() * 100) + 1); // Generate a random number
+            //console.log(randomnr);
+            numbers.push(randomnr);
+            //console.log(numbers);
+            let newId = "n-" + i;
+            //console.log(newId + " " + typeof(newId));
+            document.getElementById(newId).innerHTML = randomnr;
+        }
+        // display the smallest number
+        document.getElementById("min").innerHTML = Math.min(...numbers);
+        // display the biggest number
+        document.getElementById("max").innerHTML = Math.max(...numbers);
+        // display the sum of all numbers
+        document.getElementById("sum").innerHTML = numbers.reduce((a,b) => a+b);
+        // display their average.
+        document.getElementById("average").innerHTML = numbers.reduce((a,b) => a+b, 0) / numbers.length
+
+
+
+
+    });
 
 })();
