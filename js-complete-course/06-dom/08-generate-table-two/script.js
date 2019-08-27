d3f526616b782bfae704ddc9bdb4cc17ca05881c
bona-kim <bona.kim@outlook.be>
2019-08-27 12:26:01 +0200

    create a table

--- js-complete-course/06-dom/08-generate-table-two/script.js
+++ js-complete-course/06-dom/08-generate-table-two/script.js
@@ -11,6 +11,28 @@
 
 (function() {
 
-    // your code here
+
+    // create an HTML table with 1 column and 10 rows.
+    let target = document.getElementById("target");
+
+    let table = document.createElement("table");
+    table.setAttribute("id", "myTable"); // set id for my table
+    table.setAttribute("border",1);      // set border to see the line
+
+
+    // put the table inside the target
+    target.appendChild(table);
+
+    // Insert 10rows, 10cell inside each row
+    for(let r=0; r<10; r++){
+        // Insert a row
+        let row =document.getElementById("myTable").insertRow(r);
+        // Insert a 10cells inside the row
+        for(let c=0; c<10; c++){
+            let col = row.insertCell(c);
+            // show the multiplication results
+            col.innerHTML = (r+1) * (c+1) ;
+        }
+    }
 
 })();
