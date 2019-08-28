cf420e68315a6c162f796f3f29bf9aa536a70df3
bona-kim <bona.kim@outlook.be>
2019-08-28 12:29:34 +0200

    finish 7.1

--- js-complete-course/07-misc/01-waving-text/script.js
+++ js-complete-course/07-misc/01-waving-text/script.js
@@ -10,7 +10,61 @@
 // You will have time to focus on it later.
 
 (function() {
+    /*
+    var letterContainer = document.querySelector("#target");
+    var letters = letterContainer.innerText;
 
+    letters = letters.split("");
+    letterContainer.innerText = "";
+    var offset = 0;
+
+    letters.forEach(function(letter, i){
+        var wrap = document.createElement("span");
+        wrap.innerText = letter;
+        wrap.style.position = "relative";
+        wrap.style.bottom = i+"px";
+        wrap.style.transform = "rotate("+-i+"deg)";
+        letterContainer.appendChild(wrap);
+    })
+
+    */
     // your code here
+    let textElement = document.querySelector("#target");
+    let text = textElement.innerText;
+
+    text = text.split("");
+    //console.log(text);
+
+    textElement.innerText = "";
+
+    let i = 0;
+    text.forEach(function(letter){  // do this foor every letter in text
+
+        // wrap each letter in span & give id
+        let wrap = document.createElement("span");
+        wrap.id = i;
+        wrap.innerText = letter;
+
+        // decide font-size depends on the positon.
+        if (i % 10 === 1){
+            wrap.style.fontSize = 10+"px";
+        } else if ((i % 10 === 2) || (i % 10 === 0)){
+            wrap.style.fontSize = 15+"px";
+        } else if ((i % 10 === 3) || (i % 10 === 9)){
+            wrap.style.fontSize = 20+"px";
+        } else if ((i % 10 === 4) || (i % 10 === 8)){
+            wrap.style.fontSize = 25+"px";
+        } else if ((i % 10 === 5) || (i % 10 === 7)){
+            wrap.style.fontSize = 30+"px";
+        } else {
+            wrap.style.fontSize = 35+"px";
+        }
+
+        // append the letter
+        textElement.appendChild(wrap);
+        i++;
+    });
+
+
 
 })();
