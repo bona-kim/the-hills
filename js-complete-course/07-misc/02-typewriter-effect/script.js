b6205c5379f19c95a8417ec4b8a0f2b359ffc19f
bona-kim <bona.kim@outlook.be>
2019-08-28 18:17:11 +0200

    try again in the morning

--- js-complete-course/07-misc/02-typewriter-effect/script.js
+++ js-complete-course/07-misc/02-typewriter-effect/script.js
@@ -11,6 +11,79 @@
 
 (function() {
 
-    // your code here
+    // Display some text inside the target element.
+    // Create a "typewriter" effect by making each letter
+    // appear one after the other.
+    // Use a random delay between each letter appearing.
+
+    let container = document.getElementById("target");
+    console.log(container);
+
+    let letters = [container.innerText];
+    console.log(letters);
+    let t = -1;
+    let letter = "";
+    let message = container.innerHTML;
+    let mode = 'write';
+    let delay = 1000;
+
+    function updateText(txt) {
+        container.innerHTML = txt;
+    }
+
+    function tick(){
+        if(container.innerHTML.length === 0){
+            t++;
+            letter = letter[t];
+            message = "";
+            mode = "write";
+        }
+
+        switch(mode) {
+            case "write" :
+                message += letter.slice(0,1);
+                letter = letter.substr(1);
+                updateText(message);
+
+                if(letter.length === 0 && t === (letters.length - 1)) {
+                    window.clearTimeout(timeout);
+                    return;
+                }
+
+                if(letter.length === 0){
+                    mode = 'delete';
+                    delay = 1500;
+                } else {
+                    delay = 32 + Math.round(Math.random() * 40);
+                }
+
+                break;
+
+            case 'delete' :
+                message = message.slice(0, -1);
+                updateText(message);
+
+                if(message.length === 0)
+                {
+                    mode = 'write';
+                    delay = 1500;
+                } else {
+                    delay = 32 + Math.round(Math.random() * 100);
+                }
+                break;
+
+
+        }
+
+        timeout = window.setTimeout(tick, delay);
+
+    }
+
+
+    let timeout = window.setTimeout(tick, delay);
 
 })();
+
+
+
+// help :  https://jayhoffmann.com/how-to-make-text-that-writes-itself-in-javascript/
