2be7f19c465657cb3981a8482f2fb527e9519bfd
bona-kim <bona.kim@outlook.be>
2019-08-28 16:41:11 +0200

    finish 7.4

--- js-complete-course/07-misc/04-worst-ui-two/script.js
+++ js-complete-course/07-misc/04-worst-ui-two/script.js
@@ -11,6 +11,32 @@
 
 (function() {
 
-    // your code here
+    // get all 4 buttons
+    const buttons = document.getElementsByTagName("button");
+
+    // for Each button, will do this
+    for (let i =0; i<buttons.length; i++){
+        buttons[i].addEventListener("click", function(){
+            let minNr = buttons[i].getAttribute("data-min");
+            let maxNr = buttons[i].getAttribute("data-max");
+
+            let curNr = buttons[i].innerHTML;
+            if ( curNr === maxNr ){
+                curNr = minNr
+            } else { curNr++ }
+            console.log(curNr);
+            if( i === 0) { curNr } else { curNr = ("0" + curNr).slice(-2).toString()}
+            buttons[i].innerHTML = curNr;
+
+            document.getElementById("target").innerHTML =
+                "+" + buttons[0].innerHTML
+                + ("0"+ buttons[1].innerHTML).slice(-2).toString()
+                + ("0"+ buttons[2].innerHTML).slice(-2).toString()
+                + ("0"+ buttons[3].innerHTML).slice(-2).toString()
+        })
+
+    }
+
+
 
 })();
