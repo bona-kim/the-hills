c4dec70049e2ad0e0cf1f059bed1a4685fad8e02
bona-kim <bona.kim@outlook.be>
2019-08-29 10:27:41 +0200

    finish 7.6

--- js-complete-course/07-misc/06-guess-number/script.js
+++ js-complete-course/07-misc/06-guess-number/script.js
@@ -9,7 +9,37 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
+ (function() {
+    window.addEventListener('load', (event) => {
+
+        // generate random value and put in a var
+        let number = Math.floor((Math.random()*100) + 1);
+        //alert(number);        // to check the right number in alert
+        console.log(number);    // to check the right number in the console
+
+        // count for the number of guesses
+        let count = 1;
+
+
+        let guess;
+        do { //  asks the user till the guess is correct
+            guess = prompt("Please guess the number between 1 and 100");
+            if (guess > number){
+                alert("lower");
+                count++;                        // if the guess is wrong, add 1 to count for next guess
+            } else if  (guess < number){
+                alert("higher");
+                count++;
+            }
+        } while (guess != number);
+
+        console.log(count);
+        window.alert("That's it! you needed " + count + " guesses.")
+
+
+
+
+    });
 
     // your code here
 
