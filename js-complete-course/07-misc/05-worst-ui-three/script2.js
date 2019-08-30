e14a5021ff9ff4b651ed99d76bc95dd8c9392628
bona-kim <bona.kim@outlook.be>
2019-08-30 15:25:15 +0200

    with random number

--- js-complete-course/07-misc/05-worst-ui-three/script2.js
+++ js-complete-course/07-misc/05-worst-ui-three/script2.js
@@ -11,69 +11,25 @@
 
 (function () {
 
-    // declare the necessary variables
-    const result = document.getElementById("target");   // element for display whole number
-    const slots = document.querySelectorAll("input");    // 4slot for part of number
-    const buttons = document.querySelectorAll("button"); // all the stop buttons
+    Array.from(document.getElementsByTagName('button')).forEach(function(element){
+        element.addEventListener('click', function(){
+            console.log("clicked");
+            var inputField = element.parentElement.getElementsByTagName('input')[0];
+            var rawMin= inputField.getAttribute("data-min");
+            var rawMax = inputField.getAttribute("data-max");
+            var min = +rawMin;
+            var max = +rawMax;
 
-    // separate the displayed numbers in + and 4part
-    let number = ["460", "00", "00", "00"];
-    result.innerHTML = "+" + number[0] + number[1] + number[2] + number[3];
+            //console.log(min, max, min+max);
 
-    // create a new array with the result of MySlot for every array element(slots)
-    var buttonReferences = Array.from(slots).map(mySlots);
-    console.log('buttonReferences', buttonReferences);
+            var randomNumber = Math.floor(Math.random() * ( max -min +1 )+ min);
+            inputField.value = String(randomNumber).padStart(rawMin.length, 0);
 
-    // let number in a slot goes up automatically
-    function mySlots(slot, i) {
-        // take the min&max num
-        let minNr = Number(slot.getAttribute("data-min"));
-        let maxNr = Number(slot.getAttribute("data-max"));
-        // set the default for number with minNr
-        let num = minNr;
-        // for a leading 0
-        let sliceNr;
-        if (i === 0) {
-            sliceNr = -3;
-        } else {
-            sliceNr = -2;
-        }
-
-        // Increase number till number = maxNr
-        function increaseNum() {
-            if (num < maxNr) {
-                num++;
-                let nrWithZero = ("0" + num).slice(sliceNr).toString(); // change the number with a leading 0
-                slot.setAttribute("value", nrWithZero); // set the new number in the slot
-            } else {
-                num = minNr;    // if num reaches to maxNr, reset the number with minNR
-            }
-        }
-
-        // Keep running the function increaseNumber every 0.2 seconds°
-        return setInterval(increaseNum, 200);
-
-    }
-
-    // for all 4 button; run the function myButtons
-    buttons.forEach(myButtons);
-
-    function myButtons(button, i) {
-        // if the stop buttons are clicked,
-        button.addEventListener("click", function () {
-            // the number is fixed
-            clearInterval(buttonReferences[i]);
-            // take the fixed number and set the attr with that
-            slots[i].setAttribute("value", slots[i].value);
-            // change the value for display
-            number[i] = slots[i].value;
-            // display the phone number
-            result.innerHTML = "+" + number[0] + number[1]
-                + number[2] + number[3];
-        });
-
-
-    }
-
-
+            document.getElementById("target").innerHTML = "+0" +
+                document.getElementById("part-one").value +
+                document.getElementById("part-two").value +
+                document.getElementById("part-three").value +
+                document.getElementById("part-four").value
+        })
+    })
 })();
