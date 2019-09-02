822ac937c26e8f1f45b6893634ec8ff317696edd
bona-kim <bona.kim@outlook.be>
2019-09-02 17:31:22 +0200

    finish 9.4

--- js-complete-course/09-fetch/04-add/script.js
+++ js-complete-course/09-fetch/04-add/script.js
@@ -10,5 +10,52 @@
 // You will have time to focus on it later.
 
 (() => {
-    // your code here
+
+    // If the button is clicked
+    document.getElementById("run").addEventListener("click", function(){
+
+
+        // ask json file to the server and then Once it is ready,
+        fetch('http://localhost:63342/the-hills/js-complete-course/_shared/api.json')
+
+        // get the json. Once it is done,
+            .then(function(response){
+                return response.json()
+            })
+
+
+            // run the function
+            .then(function (data) {
+                console.log(data);   // to check data in json file
+
+                // get the id from the form
+                let inputName = document.getElementById("hero-name").value;
+                let inputAlterEgo = document.getElementById("hero-alter-ego").value;
+                let inputPower = document.getElementById("hero-powers").value;
+                console.log(inputName, inputAlterEgo, inputPower);
+
+                // create a new hero array
+                let newHero = {};
+
+                newHero['id'] = data.heroes.length +1;  // id : lengte of the data.hero + 1 //newHero.id = data.heroes.length +1
+                newHero['name'] = inputName;            // name : inputName
+                newHero['alterEgo'] = inputAlterEgo;    // Alter-Ego : inputAlterEgo
+                newHero['abilities'] = [];              // prepare the array for abilities
+                newHero.abilities.push(inputPower);     // put inputPower in array
+                // console.log(newHero);
+                data.heroes.push(newHero);              // put the New hero in a original array
+                // console.log(data);
+                // console.log(data.heroes);
+
+            });
+
+
+    });
+
+
+
+
+
+
+
 })();
